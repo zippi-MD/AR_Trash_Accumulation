4473704f4c7722ed42388d8ca546eb5081d52a03
Alejandro Mendoza <zippi180@gmail.com>
2019-06-10 11:52:53 -0500

    Initial Commit

---
+++ Santillana-Demo-AR-Experiencia-Residuos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Santillana-Demo-AR-Experiencia-Residuos
+//
+//  Created by Alejandro Mendoza on 6/10/19.
+//  Copyright © 2019 Alejandro Mendoza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
