bad5dfcea2a3b19a4a2da1bd91ac681982a698a8
Carlos1530gamer <gamercarlos1530@comunidad.unam.mx>
2019-06-11 10:42:09 -0500

    -modify info.plist

---
+++ Santillana-Demo-AR-Experiencia-Residuos/ARSceneViewController+ARSCNViewDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  ARSceneViewController+ARSCNViewDelegate.swift
+//  Santillana-Demo-AR-Experiencia-Residuos
+//
+//  Created by Carlos Daniel Hernandez Chauteco on 6/11/19.
+//  Copyright © 2019 Alejandro Mendoza. All rights reserved.
+//
+
+import ARKit
+
+extension ARSceneViewController: ARSCNViewDelegate{
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        
+    }
+}
