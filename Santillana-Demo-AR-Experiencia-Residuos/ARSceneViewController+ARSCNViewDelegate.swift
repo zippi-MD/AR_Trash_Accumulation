6f0375d14910023ef9b9a71e6340851cfdc47e21
Carlos1530gamer <gamercarlos1530@comunidad.unam.mx>
2019-06-11 12:23:17 -0500

    finish the code for detect planes

--- Santillana-Demo-AR-Experiencia-Residuos/ARSceneViewController+ARSCNViewDelegate.swift
+++ Santillana-Demo-AR-Experiencia-Residuos/ARSceneViewController+ARSCNViewDelegate.swift
@@ -7,9 +7,36 @@
 //
 
 import ARKit
+import SceneKit
 
 extension ARSceneViewController: ARSCNViewDelegate{
+    //MARK: Delegates
     func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        guard let planeAnchor = anchor as? ARPlaneAnchor, let planeNode = createPlaneNode(planeAnchor: planeAnchor) else { return }
+        node.addChildNode(planeNode)
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        guard let planeAnchor = anchor as? ARPlaneAnchor, let planeNode = createPlaneNode(planeAnchor: planeAnchor) else { return } //craete a merge geometry
+        for child in node.childNodes{
+            child.removeFromParentNode()
+        }
+        node.addChildNode(planeNode)
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didRemove node: SCNNode, for anchor: ARAnchor) {
+        for child in node.childNodes{
+            child.removeFromParentNode()
+        }
+    }
+    //MARK: functions
+    private func createPlaneNode(planeAnchor: ARPlaneAnchor) -> SCNNode?{
+        guard let device = MTLCreateSystemDefaultDevice(), let geometryOfNode = ARSCNPlaneGeometry(device: device) else { return nil }
+        geometryOfNode.update(from: planeAnchor.geometry)
+        let node = SCNNode(geometry: geometryOfNode)
+        //node.physicsBody = .static()
+        node.geometry?.firstMaterial?.diffuse.contents = UIColor.green.withAlphaComponent(0.5)
         
+        return node
     }
 }
