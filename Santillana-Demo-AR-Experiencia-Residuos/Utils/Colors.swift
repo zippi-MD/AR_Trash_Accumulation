23b38d22b2180785c6cf986e3389ac0b1d2e1d6c
Alejandro Mendoza <zippi180@gmail.com>
2019-06-12 14:04:26 -0500

    Added Santillana Color Palette

--- Santillana-Demo-AR-Experiencia-Residuos/Utils/Colors.swift
+++ Santillana-Demo-AR-Experiencia-Residuos/Utils/Colors.swift
@@ -21,4 +21,18 @@ extension UIColor {
         self.init(red:(netHex >> 16) & 0xff, green:(netHex >> 8) & 0xff, blue:netHex & 0xff)
     }
     
+    struct Santillana {
+        
+        static var green = UIColor(red: 46, green: 204, blue: 113)
+        static var red = UIColor(red: 231, green: 76, blue: 60)
+        static var blue = UIColor(red: 52, green: 152, blue: 219)
+        static var purple = UIColor(red: 155, green: 89, blue: 182)
+        static var yellow = UIColor(red: 241, green: 196, blue: 15)
+        static var orange = UIColor(red: 230, green: 126, blue: 34)
+        static var deepBlue = UIColor(red: 52, green: 73, blue: 94)
+        
+        static var colors = [green, red, blue, purple, yellow, orange, deepBlue]
+        
+        
+    }
 }
