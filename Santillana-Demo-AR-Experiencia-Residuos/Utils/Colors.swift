117278a4055fc21bfc7d3dd2511d4300c006d04c
Alejandro Mendoza <zippi180@gmail.com>
2019-06-12 13:55:34 -0500

    Added a convenience init to UIColor to easier make rgb UIColors

---
+++ Santillana-Demo-AR-Experiencia-Residuos/Utils/Colors.swift
@@ -0,0 +1,18 @@
+//
+//  Colors.swift
+//  Santillana-Demo-AR-Experiencia-Residuos
+//
+//  Created by Alejandro Mendoza on 6/12/19.
+//  Copyright © 2019 Alejandro Mendoza. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    convenience init(red: Int, green: Int, blue: Int) {
+        assert(red >= 0 && red <= 255, "Invalid red component")
+        assert(green >= 0 && green <= 255, "Invalid green component")
+        assert(blue >= 0 && blue <= 255, "Invalid blue component")
+        self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: 1.0)
+    }
+}
