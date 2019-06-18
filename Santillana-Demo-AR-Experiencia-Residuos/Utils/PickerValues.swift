dc69b92b4dc682a6248ae32872dcd07af1370ac5
Alejandro Mendoza <zippi180@gmail.com>
2019-06-17 22:45:39 -0500

    Added pickerValues file and updated TrashInformation Structure

---
+++ Santillana-Demo-AR-Experiencia-Residuos/Utils/PickerValues.swift
@@ -0,0 +1,17 @@
+//
+//  PickerValues.swift
+//  Santillana-Demo-AR-Experiencia-Residuos
+//
+//  Created by Alejandro Mendoza on 6/17/19.
+//  Copyright © 2019 Alejandro Mendoza. All rights reserved.
+//
+
+import Foundation
+
+let pickerMassValues = [Mass.kilo: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]]
+
+let pickerTimeValues = [
+                        Time.day: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31],
+                        Time.month: [1,2,3,4,5,6,7,8,9,10,11,12],
+                        Time.year: [1,2,3,4,5,6,7,8,9,10]
+                        ]
