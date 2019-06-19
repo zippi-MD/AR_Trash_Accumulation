17b9a89bcddcdc0670ea9f751d08b61c497b278d
Carlos1530gamer <gamercarlos1530@comunidad.unam.mx>
2019-06-19 16:45:14 -0500

    added function for calculate total trash and get trash per plane

---
+++ Santillana-Demo-AR-Experiencia-Residuos/Utils/CalculateTotalTrash.swift
@@ -0,0 +1,12 @@
+//
+//  CalculateTotalTrash.swift
+//  Santillana-Demo-AR-Experiencia-Residuos
+//
+//  Created by Carlos Daniel Hernandez Chauteco on 6/19/19.
+//  Copyright © 2019 Alejandro Mendoza. All rights reserved.
+//
+
+import Foundation
+func calculateTotalTrash(days: Int, trahPerDay: Int) -> Float{
+    return Float(days * trahPerDay)
+}
